32cc782dcd4e81462e63106352c380dd48eccaab
yusuf <yusuf.a.mail00@gmail.com>
2026-01-08 10:52:59 +0530

    declaration and definition

--- learning_cpp/HelloWorld/src/Main.cpp
+++ learning_cpp/HelloWorld/src/Main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 
+void log(const char*);
+
 int main(){
     std::cout << "Hello world!" <<std::endl;
+    log("using declaration to use function in this program");
+    std::cin.get();
     return 0 ;
 }
