bcd9dd75ecffd07207fd8a04359023d35d48bf17
yusuf <yusuf.a.mail00@gmail.com>
2026-01-10 10:51:08 +0530

    functionsAndlinking

--- learning_cpp/HelloWorld/src/Main.cpp
+++ learning_cpp/HelloWorld/src/Main.cpp
@@ -2,15 +2,20 @@
 using namespace std ; 
 void log(const char*);
 
-int multiply(int a , int b){
-    log("multiply");
-    return a * b; 
+ int multiply(int a , int b);//{
+//     log("multiply");
+//     return a * b; 
+// }
+void multiplyAndlog(int a , int b){
+  cout<<multiply(a,b)<<endl;
 }
 
 int main(){
     cout << "Hello world!" <<endl;
     log("using declaration to use function in this program");
-    cout << multiply(5,3)<<endl;
+    multiplyAndlog(3,2);
+    multiplyAndlog(8,5);
+    multiplyAndlog(90,45);
   //  std::cin.get(); used for the console in windows to stay a little bit longer 
     return 0 ;
 }
